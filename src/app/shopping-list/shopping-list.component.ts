1a755cde32d6b5feddf01d839ea53a73de398868
Heena Dave <hari331996@gmail.com>
2020-03-20 00:32:28 +0530

    Shopping List & Edit

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomatoes', 5),
+    new Ingredient('Maize', 2),
+    new Ingredient('Refined flour', 500)
+  ];
 
   constructor() { }
 
